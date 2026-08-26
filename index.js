8d9bdd6221fd554a01250430f09b172550b3ad29
alimoamed895-ctrl <alimoamed895@gmail.com>
2026-08-26 18:28:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-import { readFile } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
+export default function handler(req, res) {
+  res.statusCode = 302;
 
-export default async function handler(req, res) {
-  try {
-    const currentDir = path.dirname(
-      fileURLToPath(import.meta.url)
-    );
+  res.setHeader(
+    "Location",
+    "/index.html"
+  );
 
-    const html = await readFile(
-      path.join(currentDir, "index.html"),
-      "utf8"
-    );
-
-    res.statusCode = 200;
-
-    res.setHeader(
-      "Content-Type",
-      "text/html; charset=utf-8"
-    );
-
-    res.end(html);
-
-  } catch (error) {
-    console.error(error);
-
-    res.statusCode = 500;
-
-    res.setHeader(
-      "Content-Type",
-      "text/plain; charset=utf-8"
-    );
-
-    res.end(
-      "Education GPT: فشل تحميل الصفحة."
-    );
-  }
+  res.end();
 }
